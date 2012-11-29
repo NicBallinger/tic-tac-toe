f8d9a1dac605d862c03db71aaf8eef48ebd58c88
Matthew Smith <m0smith@yahoo.com>
2012-11-29 10:05:46 -0700

    Added a lanterna ui for a curses like screen

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject tic-tac-toe "1.0.0-SNAPSHOT"
   :description "Tic Tac Toe"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [seesaw "1.4.2"]])
+                 [seesaw "1.4.2"]
+                 [clojure-lanterna "0.9.2"]])
