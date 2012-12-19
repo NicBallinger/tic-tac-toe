d3a24f4acd950be0b7c365100c32849577e84e2f
Matthew Smith <m0smith@yahoo.com>
2012-12-19 15:57:14 -0700

    renamed lein-stencil to lein-resouce

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
 (defproject tic-tac-toe "1.0.0-SNAPSHOT"
-  :description "Tic Tac Toe"
+  :description "The classic tic-tac-toe game written in Clojure. This project isn't so much about tic-tac-toe but more about Clojure technology and how to write a code base that will run in multiple user interfaces. The tic-tac-toe games presents a small but interesting problem domain."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [seesaw "1.4.2"]
                  [clojure-lanterna "0.9.2"]]
-  :plugins [[lein-cljsbuild "0.2.9"] [lein-stencil "0.1.0"] ]
+  :plugins [[lein-cljsbuild "0.2.9"] [lein-resource "0.1.1"] ]
   :hooks [leiningen.cljsbuild]
   :source-path "src"
   :url "http://m0smith.freeshell.org/tictactoe.html"
-  :prep-tasks ["javac" "compile" "stencil"]
-  :stencil {:resource-paths ["src-stencil"] ;; required or does nothing
+  :prep-tasks ["javac" "compile" "resource"]
+  :resource {:resource-paths ["src-stencil"] ;; required or does nothing
             :target-path "target/html"      ;; optional default to the global one
             :extra-values { :year ~(.get (java.util.GregorianCalendar.)
                                          (java.util.Calendar/YEAR)) }  ;; optional - default to nil
