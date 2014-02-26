a48d09abf3e3b9e9013f3943d994d6630783a624
Matthew O. Smith <matt@m0smith.com>
2014-02-26 08:55:41 -0700

    Updated dependencies like clojure 1.5.1, cljs-build and lein-resource

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject tic-tac-toe "1.0.0-SNAPSHOT"
+(defproject tic-tac-toe "1.0.1"
   :description "The classic tic-tac-toe game written in Clojure. This project isn't so much about tic-tac-toe but more about Clojure technology and how to write a code base that will run in multiple user interfaces. The tic-tac-toe games presents a small but interesting problem domain."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0-RC1"]
-                 [seesaw "1.4.2"]
-                 [clojure-lanterna "0.9.2"]]
-  :plugins [[lein-cljsbuild "0.2.9"] [lein-resource "0.3.3"] ]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.4"]
+                 [clojure-lanterna "0.9.4"]]
+  :plugins [[lein-cljsbuild "1.0.2"] [lein-resource "0.3.4"] ]
   :hooks [leiningen.cljsbuild leiningen.resource]
   :source-path "src"
   :url "http://m0smith.freeshell.org/tictactoe.html"
@@ -13,8 +13,8 @@
   :resource {:resource-paths ["src-stencil"] ;; required or does nothing
              :target-path "target/html"      ;; optional default to the global one
              :excludes [#".*~"]
-            :extra-values { :year ~(.get (java.util.GregorianCalendar.)
-                                         (java.util.Calendar/YEAR)) }  ;; optional - default to nil
+             :extra-values { :year ~(.get (java.util.GregorianCalendar.)
+                                          (java.util.Calendar/YEAR)) }  ;; optional - default to nil
            }
   ;; :filespecs {:type :fn
   ;;             :fn (fn [p]
@@ -22,11 +22,13 @@
   ;;                    :path +"inside-jar-path"
   ;;                    :bytes (format ...)})}
   :cljsbuild {
-              :builds [{:source-path "src-cljs"
-                        :compiler {:output-to "target/html/tictactoe.js"
-                                   :optimizations :advanced
-                                   :pretty-print true}}]
-              :crossovers [tic-tac-toe.core tic-tac-toe.ai.random]
+              :builds [{:source-paths ["src-cljs"],
+                        :compiler
+                        {:pretty-print true,
+                         :output-to "target/html/tictactoe.js",
+                         :optimizations :advanced}}],
+              :crossovers [tic-tac-toe.core tic-tac-toe.ai.random]}
+
   }
   :main tic-tac-toe.ui.tty
   :pom-addition [:developers [:developer
